8dbff3c996a8bdf8f144af702e00729750921f42
RabiiulMannanSajed <msajed211086@bscse.uiu.ac.bd>
2022-12-01 02:04:44 +0600

    qr-code first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Favicon from 'react-favicon';
+import icon from '../src/images/favicon-32x32.png'
+import qrCode from '../src/images/image-qr-code.png';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <Favicon url={icon} />
+      </div>
+      <div className='grid justify-items-center mt-10 '>
+        <div className=" w-96 bg-base-100 shadow-xl rounded-md ">
+          <figure className='p-5'><img className='rounded-md' src={qrCode} alt="Shoes" /></figure>
+          <div className="card-body justify-items-center">
+            <h2 className="card-title">Improve Your front-end Skills by building project</h2>
+            <p>Scan the QR code to visit frontend Mentor and take you coding skills to the next level</p>
+
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
